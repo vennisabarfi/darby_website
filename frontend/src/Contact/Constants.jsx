8a658fdaffe9f1d37b63d648ad03b38e80058d56
veyhn <vennisabarfi@gmail.com>
2024-12-24 20:17:21 -0500

    updated to use .env here and exported constants to one file

---
+++ frontend/src/Contact/Constants.jsx
@@ -0,0 +1,4 @@
+export const DARBY_EMAIL = import.meta.env.VITE_DARBY_EMAIL;
+export const DARBY_LINKEDIN = import.meta.env.VITE_DARBY_LINKEDIN;
+export const VENNISA_WEBSITE = import.meta.env.VITE_VENNISA_WEBSITE;
+
