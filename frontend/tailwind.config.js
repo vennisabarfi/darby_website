abbaacdb28fcab2fbe540eb5c406d507c89ccdf4
unknown <vennisabarfi@gmail.com>
2024-12-27 18:30:31 -0500

    fixed tailwind config issues with animate and require

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
     darkMode: ["class"],
     content: [
@@ -15,6 +17,7 @@ export default {
   		colors: {}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+
+  plugins: [tailwindcssAnimate],
 }
 
